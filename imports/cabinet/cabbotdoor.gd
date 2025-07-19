9657331b2c6ae5add62fdb3d6c715181f98d7f4d
Solaros711 <spurdy885@gmail.com>
2025-07-19 12:43:09 -0700

    added cabinet, started working on animation tree tests

---
+++ imports/cabinet/cabbotdoor.gd
@@ -0,0 +1,23 @@
+extends StaticBody3D
+
+var toggle = true
+var interactable = true
+
+@export var animation_player: AnimationPlayer
+
+func interact():
+	if interactable == true:
+		interactable = false
+		toggle = !toggle
+		if toggle == false:
+			animation_player.play("bot_door_open")
+			print("opening door!")
+		if toggle == true:
+			animation_player.play("bot_door_close")
+			print("closing door!")
+		await get_tree().create_timer(0.5, false).timeout
+		interactable = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

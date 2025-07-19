95f74f94a9998ed01e26a2d2a88f2ee90d49a2e7
Solaros711 <spurdy885@gmail.com>
2025-07-19 13:51:33 -0700

    figured out animation tree is useful for persons, and animation blending, not static objects lmao

--- imports/cabinet/cabtopdoor.gd
+++ imports/cabinet/cabtopdoor.gd
@@ -4,16 +4,19 @@ var toggle = true
 var interactable = true
 
 @export var animation_player: AnimationPlayer
+@export var animation_tree: AnimationTree
 
 func interact():
 	if interactable == true:
 		interactable = false
 		toggle = !toggle
 		if toggle == false:
-			animation_player.play("top_door_open")
+			#animation_player.play("top_door_open")
+			animation_tree.set("Parameters/Top_Door/BlendPosition", 0.1)
 			print("opening door!")
 		if toggle == true:
-			animation_player.play("top_door_close")
+			#animation_player.play("top_door_close")
+			animation_tree.set("Parameters/Top_Door/BlendPosition", -0.1)
 			print("closing door!")
 		await get_tree().create_timer(0.5, false).timeout
 		interactable = true

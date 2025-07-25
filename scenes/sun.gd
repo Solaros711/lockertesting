93be4b3098e9b28931891714597c3cbd947e1753
Solaros711 <spurdy885@gmail.com>
2025-07-24 17:08:03 -0700

    Adjusted sun position and shadow lighting

--- scenes/sun.gd
+++ scenes/sun.gd
@@ -1,24 +1,27 @@
 extends DirectionalLight3D
 var time = 0.0
 #end of day occurs as 3.142 lmao
-const day_color = Color(1.0, 0.4705, 0, 1.0)
+const DAY_COLOR = Color(1.0, 0.4705, 0, 1.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	light_color = day_color
+	light_color = DAY_COLOR
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(rotation.x > 0.0):
+	do_sun_move()
+
+#rotates the sun on the x axis, changing the color based on where it is on the horizon
+func do_sun_move():
+	if(rotation.x > 0.10472):
 		rotate_x(-0.0005)
 		time = 0.0
 		light_energy = 0.0
 	else:
 		light_energy = 1.0
 		if time == 0.0:
-			light_color = day_color
+			light_color = DAY_COLOR
 		#Turns sun color from dawn to day
 		if time < 1.0:
 			var sun_color = light_color
@@ -28,7 +31,7 @@ func _process(delta: float) -> void:
 				sun_color.b = sun_color.b + 0.0005
 			light_color = sun_color
 		#Turns sun color from day to dusk
-		if time > 2.542:
+		if time > 2.642:
 			var sun_color = light_color
 			if sun_color.g > 0.4705:
 				sun_color.g = sun_color.g - 0.0005
